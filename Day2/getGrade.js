e3aa96eceafd307eef60face91ce022a905c278f
Dove-White <dugbazahredeemer@gmail.com>
2023-09-25 12:18:33 +0000

    changes on getGrade function

--- Day2/getGrade.js
+++ Day2/getGrade.js
@@ -1,14 +1,19 @@
 function getGrade(score) {
-    let grade;
-    // Write your code here
-    if(score <= 30) {grade = 'A'}
-    if(score <= 25) {grade = 'B'}
-    if(score <= 20) {grade = 'C'}
-    if(score <= 15) {grade = 'D'}
-    if(score <= 10) {grade = 'E'}
-    if(score <= 5) {grade = 'F'}
-    
-    return grade;
+  let grade;
+  // Write your code here
+  score > 29
+    ? (grade = "A")
+    : score > 24
+    ? (grade = "B")
+    : score > 19
+    ? (grade = "C")
+    : score > 14
+    ? (grade = "D")
+    : score > 9
+    ? (grade = "E")
+    : (grade = "F");
+
+  return grade;
 }
 
 console.log(getGrade(20));
